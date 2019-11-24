70d2d1cbc186ad1bc00f5737cc6be1e134465a5a
Nick Pappas <nick@radicand.org>
2019-11-24 08:22:53 -0600

    update build scripts

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -8,4 +8,5 @@ VCS_VERSION=$(git describe)
 
 docker buildx build --platform linux/arm/v7	--build-arg VCS_REF=$VCS_REF --build-arg VCS_VERSION=$VCS_VERSION -f Dockerfile -t radicand/docker-mailserver:`git rev-parse --verify --short HEAD` -t radicand/docker-mailserver:latest .
 
-#docker push radicand/docker-mailserver
+docker push radicand/docker-mailserver:`git rev-parse --verify --short HEAD`
+docker push radicand/docker-mailserver:latest
